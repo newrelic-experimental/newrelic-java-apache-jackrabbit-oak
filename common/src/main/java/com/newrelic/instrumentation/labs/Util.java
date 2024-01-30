3ed36490a05337b8e5dd3995333a09d94496470d
Scott DeWitt <sdewitt@newrelic.com>
2024-01-30 09:55:44 -0500

    feat: add instrumentation for oak-core

---
+++ common/src/main/java/com/newrelic/instrumentation/labs/Util.java
@@ -0,0 +1,15 @@
+package com.newrelic.instrumentation.labs;
+
+import java.util.Map;
+
+public class Util {
+	public static void recordValue(
+		Map<String,Object> attributes,
+		String key,
+		Object value
+	) {
+		if(key != null && !key.isEmpty() && value != null) {
+			attributes.put(key, value);
+		}
+	}
+}
