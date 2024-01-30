4373636b206a673f2592be2bc1f57eaca0e729fa
Scott DeWitt <sdewitt@newrelic.com>
2024-01-30 16:34:27 -0500

    feat/instrument-oak-core: remove unused import

--- oak-core/src/main/java/org/apache/jackrabbit/oak/query/Query_Weaved.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/query/Query_Weaved.java
@@ -9,7 +9,6 @@ import org.apache.jackrabbit.oak.api.Result;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.logging.Level;
 
 @Weave(
 	originalName = "org.apache.jackrabbit.oak.query.Query",
