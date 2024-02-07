521f38372461635f98e2b36772aa7d27156bee25
Scott DeWitt <sdewitt@newrelic.com>
2024-02-07 14:18:32 -0500

    chore: factor out constants to fix IllegalAccessException

--- oak-blob/src/main/java/org/apache/jackrabbit/oak/spi/blob/BlobStore.java
+++ oak-blob/src/main/java/org/apache/jackrabbit/oak/spi/blob/BlobStore.java
@@ -5,34 +5,11 @@ import com.newrelic.api.agent.Trace;
 import com.newrelic.api.agent.weaver.MatchType;
 import com.newrelic.api.agent.weaver.Weave;
 import com.newrelic.api.agent.weaver.Weaver;
+import com.newrelic.instrumentation.labs.Constants;
 import com.newrelic.instrumentation.labs.Instrument;
 
 import java.io.IOException;
 import java.io.InputStream;
-
-interface Constants {
-	String CLASS_NAME = BlobStore.class.getSimpleName();
-
-	String[] READ_BLOB_NAME = new String[] {
-		"Custom",
-		"JackRabbit",
-		"Oak",
-		"Spi",
-		"Blob",
-		CLASS_NAME,
-		"readBlob"
-	};
-
-	String[] WRITE_BLOB_NAME = new String[] {
-		"Custom",
-		"JackRabbit",
-		"Oak",
-		"Spi",
-		"Blob",
-		CLASS_NAME,
-		"writeBlob"
-	};
-}
 
 @Weave(type = MatchType.Interface)
 public class BlobStore {
@@ -48,13 +25,16 @@ public class BlobStore {
 			NewRelic.getAgent().getTracedMethod()
 		)
 			.name(Constants.READ_BLOB_NAME)
-			.attr("oakBlobId", blobId)
-			.attr("oakBlobReadPosition", pos)
-			.attr("oakBlobReadOffset", off)
-			.attr("oakBlobReadLength", length);
+			.attr(Constants.OAK_BLOB_ID_ATTR_NAME, blobId)
+			.attr(Constants.OAK_READ_BLOB_POS_ATTR_NAME, pos)
+			.attr(Constants.OAK_READ_BLOB_OFFSET_ATTR_NAME, off)
+			.attr(Constants.OAK_READ_BLOB_LENGTH_ATTR_NAME, length);
 
 		if (buff != null) {
-			instrument.attr("oakBlobReadBufferLength", buff.length);
+			instrument.attr(
+				Constants.OAK_READ_BLOB_BUF_LENGTH_ATTR_NAME,
+				buff.length
+			);
 		}
 
 		instrument.record();
@@ -79,7 +59,10 @@ public class BlobStore {
 			.name(Constants.WRITE_BLOB_NAME);
 
 		if (options != null) {
-			instrument.attr("oakBlobWriteUploadType", options.getUpload());
+			instrument.attr(
+				Constants.OAK_WRITE_BLOB_UPLOAD_TYPE_ATTR_NAME,
+				options.getUpload().name()
+			);
 		}
 
 		instrument.record();
